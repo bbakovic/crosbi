ff5c3e7e88cd00862f9b860718cc6c2daf9d721e
Božidar Baković <bbakovi@gmail.com>
2017-04-28 18:36:42 +0200

    v 0.2

--- test.py
+++ test.py
@@ -1,28 +1,24 @@
 from bs4 import BeautifulSoup
-import csv, urllib2, codecs, re, operator
-broj_krivih=0
-zapis=codecs.open("cro1.txt",'w','utf-8')
-for a in range(99,150):
-
+import urllib2, codecs, re, operator, pymongo
+broj_krivih = 0
+zapis = codecs.open("cro1.txt", 'w', 'utf-8')
+for a in range(99, 150):
     try:
         response = urllib2.urlopen("http://beta.bib.irb.hr/"+str(a))
-        soup = BeautifulSoup(response,"lxml")
+        soup = BeautifulSoup(response, "lxml")
         try:
             bib = soup.find_all('div', class_='container')
-            bib2=bib[2].find_all('p', class_='item-label')
+            bib2 = bib[2].find_all('p', class_='item-label')
             zapis.write("BibID \t" + str(a) + '\n')
             for x in bib2:
-                #print x,"++++++"
-                #print type(x)
-                tag=x.find('strong')
-                text=re.findall("br/>(.+?)</p", str(x).replace('\n',''))
-
+                tag = x.find('strong')
+                text = re.findall("br/>(.+?)</p", str(x).replace('\n', ''))
                 zapis.write(str(tag).lstrip("<strong>").rstrip("</strong>")+'\t'+str(text[0]).strip()+'\n')
-            zapis.write("\nnew_DB_entry\n")
-                #print str(tag).lstrip("<strong>").rstrip("</strong>"), text[0]
+            zapis.write("new_DB_entry\n")
         except AttributeError:
             broj_krivih += 1
     except urllib2.HTTPError, error:
         contents = error.read()
-    print a
+
+
 
